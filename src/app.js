d7150d7438879e5f654759fcdc359d266797510b
Jose Luis Vela <jlvela94@gmail.com>
2025-05-12 11:37:05 +0000

    Terminado lógica del generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let finalExcuse = "";
+
+  function getExcuse() {
+
+    
+
+    let excuse1 = who[Math.floor((Math.random() * 4))];
+    let excuse2 = action[Math.floor((Math.random() * 4))];
+    let excuse3 = what[Math.floor((Math.random() * 3))];
+    let excuse4 = when[Math.floor((Math.random() * 5))];
+
+    finalExcuse = excuse1 + " " + excuse2 + " " + excuse3 + " " + excuse4;
+
+  } 
+
+  getExcuse()
+  let title = document.getElementById("excuse")
+  title.innerHTML = finalExcuse
 };
